8b61b0e79e866c5e9c6f79d6001885ad0bc03e3c
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2021-11-13 21:06:20 +0530

    Small changes

--- Student/tutorList.php
+++ Student/tutorList.php
@@ -9,42 +9,43 @@ unset($_SESSION['tutor_id']);
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Tutors</title>
-</head>
-<body>
-<?php require_once "navbar.php"; ?>
-<div class="container">
-    <h1>All Tutors</h1>
-    <?php
-    $stmt = $pdo->query("SELECT id, first_name, last_name FROM `User` WHERE usertype_id=2;");
-    if (isset($_SESSION['name'])){
-        echo '<p><a href="login.php">Please log in</a></p>';
+    <head>
+        <title>Tutors</title>
+    </head>
 
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
-            echo '<div class="text-end" >';
-            echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a>');
-            echo '</div>';
-            echo '<hr>';
-        }
+    <body>
+        <?php require_once "navbar.php"; ?>
+        <div class="container">
+            <h1>All Tutors</h1>
+            <?php
+                $stmt = $pdo->query("SELECT id, first_name, last_name FROM `User` WHERE usertype_id=2;");
+                if (isset($_SESSION['name'])){
+                    echo '<p><a href="login.php">Please log in</a></p>';
 
-    } else {
-        echo'<a href="logout.php">Logout</a><br>';
+                    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
+                        echo '<div class="text-end" >';
+                        echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a>');
+                        echo '</div>';
+                        echo '<hr>';
+                    }
 
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
-            echo '<div class="text-end" >';
-            echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a> &emsp;');
-            echo ('<a href="../User/message.php?tutor_id='.$row['id'].'">Message</a> &emsp;');
-            echo ('<a href="sendRequest.php?tutor_id='.$row['id'].'">Send enrolment request</a>');
-            echo '</div>';
-            echo '<hr>';
-        }
+                } else {
+                    echo'<a href="logout.php">Logout</a><br>';
 
-    }
+                    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
+                        echo '<div class="text-end" >';
+                        echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a> &emsp;');
+                        echo ('<a href="../User/message.php?tutor_id='.$row['id'].'">Message</a> &emsp;');
+                        echo ('<a href="sendRequest.php?tutor_id='.$row['id'].'">Send enrolment request</a>');
+                        echo '</div>';
+                        echo '<hr>';
+                    }
 
-    ?>
-</div>
-</body>
+                }
+
+            ?>
+        </div>
+    </body>
 </html>
--- User/message.php
+++ User/message.php
@@ -38,42 +38,42 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Message</title>
-</head>
-<body>
-<?php require_once "../Student/navbar.php"; ?>
-<div class="container">
-    <?php
-    $sql = "SELECT first_name, last_name FROM `User` WHERE id = :tutor_id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':tutor_id' => $_SESSION['tutor_id']));
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo ('<a href="../Student/tutorList.php">Back</a> &emsp;'.htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
+    <head>
+        <title>Message</title>
+    </head>
 
-    echo '<div class="text-end">';
-    echo '<a href="logout.php" class="text-end">Logout</a><br>';
-    echo '</div><hr>';
+    <body>
+        <div class="container">
+            <?php
+                $sql = "SELECT first_name, last_name FROM `User` WHERE id = :tutor_id";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute(array(':tutor_id' => $_SESSION['tutor_id']));
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                echo ('<a href="../Student/tutorList.php">Back</a> &emsp;'.htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
 
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
-        echo '<div class="text-end" >';
-        echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a> &emsp;');
-        echo ('<a href="../User/message.php?tutor_id='.$row['id'].'">Message</a> &emsp;');
-        echo ('<a href="sendRequest.php?tutor_id='.$row['id'].'">Send enrolment request</a>');
-        echo '</div>';
-        echo '<hr>';
-    }
+                echo '<div class="text-end">';
+                echo '<a href="logout.php" class="text-end">Logout</a><br>';
+                echo '</div><hr>';
 
-    ?>
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo (htmlentities($row['first_name']).' '.htmlentities($row['last_name']));
+                    echo '<div class="text-end" >';
+                    echo ('<a href="../tutor/viewDetails.php?tutor_id='.$row['id'].'">View details</a> &emsp;');
+                    echo ('<a href="../User/message.php?tutor_id='.$row['id'].'">Message</a> &emsp;');
+                    echo ('<a href="sendRequest.php?tutor_id='.$row['id'].'">Send enrolment request</a>');
+                    echo '</div>';
+                    echo '<hr>';
+                }
 
-    <form method="POST" class="row mb-3">
-        <div class="col-sm-10">
-            <input type="text" class="form-control" name="message" id="message" placeholder="Message">
+            ?>
+
+            <form method="POST" class="row mb-3">
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="message" id="message" placeholder="Message">
+                </div>
+                    <button type="submit" name="send" class="btn btn-primary col-sm-2">Send</button>
+            </form>
+
         </div>
-            <button type="submit" name="send" class="btn btn-primary col-sm-2">Send</button>
-    </form>
-
-</div>
-</body>
+    </body>
 </html>
