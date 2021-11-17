d474ec02bc915c117f2863bab67b4e7b68401969
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2021-11-17 08:58:28 +0530

    Tutor can accept and reject requests.

--- tutor/requests.php
+++ tutor/requests.php
@@ -2,6 +2,7 @@
     session_start();
     require_once "../classes/DBConn.class.php";
     require_once "../classes/Tutor.class.php";
+    require_once "../classes/Student.class.php";
 
     if (!isset($_SESSION['user_id'])){
         header("location: ../index.php");
@@ -24,17 +25,18 @@
                 } else {
                     foreach ($requests as $request) {
                         $subject = Subject::getInstance($request->getSubjectId());
-                        $sender = $request->getSenderId();
+                        $senderId = $request->getSenderId();
                         if ($request->getType() == 0){
                             $note = 'Individual request';
-
+                            $sender = Student::getInstance(Student::getUserId($senderId));
+                            $name = $sender->getFname().' '.$sender->getLname();
                         } else {
                             $note = 'Group request';
                         }
                         echo '<div class="card mx-auto rounded-3 border-0 shadow my-3">
                                       <div class="card-body">
                                         <h5 class="card-title">' . htmlentities($subject->getName()).': Grade '.htmlentities($subject->getGrade()).', '.htmlentities($subject->getMedium()).' medium</h5>
-                                        <h6 class="card-subtitle mb-2 text-muted">'.$note.' by '.$sender.'</h6>
+                                        <h6 class="card-subtitle mb-2 text-muted">'.$note.' by '.$name.'</h6>
                                             <a href="viewRequest.php?reqId='.$request->getId().'"><button class="btn btn-primary btn-sm" >View</button></a>
                                       </div>
                                 </div>';
