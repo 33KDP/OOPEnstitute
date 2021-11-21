80f9f969662cefbd8186312de5dd6b00ed541fb7
your_username <your_email_address@example.com>
2021-11-21 15:35:10 +0530

    tutpr navbar  completed

--- tutor/navbar.php
+++ tutor/navbar.php
@@ -1,5 +1,7 @@
 <?php
     require_once "../bootstrap.php";
+    $userid = $_SESSION['user_id'];
+    $curTutor=  Tutor::getInstance($_SESSION['user_id']);
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
@@ -12,9 +14,10 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Name
+                        <?=htmlentities($curTutor->getFName()." ".$curTutor->getLName())?>
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="../tutor/home.php">Home</a></li>
                         <li><a class="dropdown-item" href="../tutor/subjects.php">Subjects</a></li>
                         <li><a class="dropdown-item" href="../tutor/profile.php">Profile settings</a></li>
                         <li><hr class="dropdown-divider"></li>
