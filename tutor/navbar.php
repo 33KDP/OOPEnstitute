113133f223b786167b3a54bd123e58d24b67d111
your_username <your_email_address@example.com>
2021-11-21 15:47:14 +0530

    set home on logo

--- tutor/navbar.php
+++ tutor/navbar.php
@@ -6,12 +6,12 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="../assets/img/logo2.png" alt="E-nstitute logo" style="width:200px;height:40px;"></a>
+        <a class="navbar-brand"  href="../tutor/home.php"><img src="../assets/img/logo2.png" alt="E-nstitute logo" style="width:200px;height:40px;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <?=htmlentities($curTutor->getFName()." ".$curTutor->getLName())?>
@@ -25,7 +25,6 @@
                     </ul>
                 </li>
             </ul>
-
         </div>
     </div>
 </nav>
